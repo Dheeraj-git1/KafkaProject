520551d4b0a244fc87a2e705ff1e11b5e5065338
Dheeraj <dheeruroyal926@gmail.com>
2026-05-29 15:15:54 +0530

    instance one

--- Singleton.java
+++ Singleton.java
@@ -2,7 +2,7 @@ public class Singleton {
     private static volatile Singleton instance;
 
     private Singleton() {}
-
+    //only one instance
     public static Singleton getInstance() {
         if (instance == null) {
             synchronized (Singleton.class) {
