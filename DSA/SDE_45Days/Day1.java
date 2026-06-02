bacdbd91093e4a8708fd79d79dadec3801c5cfe1
Dheeraj <dheeruroyal926@gmail.com>
2026-06-03 00:10:49 +0530

    Matrix zero row and col

---
+++ DSA/SDE_45Days/Day1.java
@@ -0,0 +1,57 @@
+package DSA.SDE_45Days;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day1 {
+
+    // Input: matrix=[[1,1,1],[1,0,1],[1,1,1]]
+    // Output: [[1,0,1],[0,0,0],[1,0,1]]
+    // Explanation: Since matrix[2][2]=0.Therfore the 2nd column and 2nd row wil be
+    // set to 0.
+
+    public static void matrixZero() {
+
+        int[][] matrix = {
+                { 1, 1, 1 },
+                { 1, 0, 1 },
+                { 1, 1, 1 }
+        };
+
+        int[] row = new int[matrix.length];
+        int[] col = new int[matrix[0].length];
+
+        // Mark rows and columns
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        // Set zeroes
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Print matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        matrixZero();
+    }
+}
